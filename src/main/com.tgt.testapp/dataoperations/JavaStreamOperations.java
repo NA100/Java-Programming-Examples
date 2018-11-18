d8de53e050dd3acbd758d6ba90efc394130fc085
Yagna Talakola <talakola.yagna@gmail.com>
2018-11-18 12:49:05 -0600

    stream operations on list of objects

--- src/main/com.tgt.testapp/dataoperations/JavaStreamOperations.java
+++ src/main/com.tgt.testapp/dataoperations/JavaStreamOperations.java
@@ -1,44 +1,98 @@
 package com.tgt.testapp.dataoperations;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.tgt.testapp.streams.Gender;
+import com.tgt.testapp.streams.Person;
+
+import java.lang.reflect.Array;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class JavaStreamOperations {
 
     public static void main(String args[]) {
 
-        List<Integer> list = new ArrayList<>();
-        for(int i = 1; i< 10; i++){
-            list.add(i);
-        }
+        Person person1 = new Person("Achala,", Gender.FEMALE, 10000);
+        Person person2 = new Person("Neha", Gender.FEMALE, 20000);
+        Person person3 = new Person("Yagna", Gender.MALE, 23000);
+        Person person4 = new Person("Avinash,", Gender.MALE, 30000);
+        Person person5 = new Person("Khushboo", Gender.FEMALE, 36000);
 
-        //Question-1 Print all even numbers
-        //old way
-        for(int i : list) {
-            if(i % 2 == 0) {
-                System.out.println(i);
+        List<Person> personList = new ArrayList<>();
+
+        personList.add(person1);
+        personList.add(person2);
+        personList.add(person3);
+        personList.add(person4);
+        personList.add(person5);
+
+        //Question-2 Given a list of Persons, print all female name starting with 'A'
+        // without streams
+        for(Person person : personList) {
+            if (person.getName().startsWith("A") && person.getGender() == Gender.FEMALE) {
+                System.out.println(person.toString());
+            } else {
+                continue;
             }
         }
-        //new way
-        List<Integer> evenNumbersList = list.stream().filter(i -> i%2 == 0).collect(Collectors.toList());
-        System.out.print(evenNumbersList);
-        
 
-        //Question-2 Given a list of words, change them to upper case
+
+        // With streams
+        List<Person> femaleAList = personList.stream().filter(person -> person.getGender() == Gender.FEMALE)
+               .filter(person -> person.getName().startsWith("A"))
+                .collect(Collectors.toList());
+
+        for (Person person: femaleAList) {
+            System.out.println(person.toString());
+        }
+
+
+        //Question-3  Given list of Persons, convert the Arraylist to a Hashmap (key:name, value: gender)
         //old way
-        //new way
-        //hint use a map for new way
+        Map<String, Gender> personMap = new HashMap<>();
+        for (Person person : personList) {
+            personMap.put(person.getName(), person.getGender());
+        }
 
-        //Question-3  Given a list of numbers, multiply each number by 2 and print them
+        //new way
+        Map<String, Gender> newPersonMap = personList.stream().collect(Collectors.toMap(Person::getName, Person::getGender));
+
+
+        //Question-4 Find the name of the female Person getting the highest salary.
         //old way
-        //new way
-        //hint use a map for new way
+        int highestSalary = 0;
+        String femaleWithHighestSalary = "";
 
-        //Question-4  Given a list of words, return true if they contain letter "a"
-        //old way
-        //new way
-        //hint use a anyMatch for new way
+        for (Person person : personList) {
+            if (person.getSalary() > highestSalary) {
+                highestSalary = person.getSalary();
+                femaleWithHighestSalary = person.getName();
+            }
+        }
 
+        System.out.println("Female person with highest salary(old): " + femaleWithHighestSalary);
+
+        //new way
+        Comparator<Person> comparison = Comparator.comparing(Person::getSalary);
+        Person femaleWithHighestSal = personList.stream().filter(person -> person.getGender() == Gender.FEMALE).max(comparison).get();
+
+        System.out.println("Female person with highest salary(new): " + femaleWithHighestSal.getName());
+
+        // Q5 Print the total salary of all male Persons.
+        // old way
+        int totalMaleSalary = 0;
+        for (Person person : personList) {
+            if (person.getGender() == Gender.MALE) {
+                totalMaleSalary += person.getSalary();
+            }
+        }
+
+        System.out.println("total salary of all male persons(old): " + totalMaleSalary);
+
+        // new way
+        int totalMSalary = 0;
+        totalMSalary = personList.stream().filter(person -> person.getGender() == Gender.MALE)
+                .map(Person::getSalary).reduce(0, (x, y) -> x + y);
+
+        System.out.println("total salary of all male persons(new): " + totalMSalary);
     }
 }
